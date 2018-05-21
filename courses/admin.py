6c6e0e4e017a9dc8cc04529d0c8828d21c6cb0cf
Jeremy Chen <jeremy886@gmail.com>
2018-05-21 17:05:38 +1000

    Add Course and StudyGroup models to Admin.

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, StudyGroup
+
+
+admin.site.register(Course)
+admin.site.register(StudyGroup)
--- courses/models.py
+++ courses/models.py
@@ -6,9 +6,14 @@ class Course(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
 
 
 class StudyGroup(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     next_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
